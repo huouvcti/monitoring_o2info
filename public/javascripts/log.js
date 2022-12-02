64f8ca395f716b9c167e503f592f03b95511931a
huouvcti <huouvcti@gmail.com>
2022-12-02 17:12:09 +0900

    fix

--- public/javascripts/log.js
+++ public/javascripts/log.js
@@ -447,7 +447,7 @@ const log_highchart = (container, sensor_data, date, title, unit, color) => {
                 }
             },
 
-            tickInterval: Math.round(sensor_data_length*0.5),
+            tickInterval: Math.round(sensor_data_length*0.1),
             // tickLength: 100
         },
 
