0a2d7ee8c62483c5c77886fe63549b3f719b3ead
huouvcti <huouvcti@gmail.com>
2023-02-02 16:26:38 +0900

    ttt

--- middleware/ffmpeg.js
+++ middleware/ffmpeg.js
@@ -5,9 +5,11 @@ stream = new Stream({
   name: 'foscam_stream',
   streamUrl: streamUrl,
   wsPort: 9999,
-//   width: 1080,
-//   height: 800
+//   width: ,
+//   height: 100
 });
 
 
 // ffmpeg -rtsp_transport tcp -i rtsp://210.99.70.120:1935/live/cctv009.stream -vcodec libx264 -hls_flags delete_segments  test.m3u8
+
+// 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
