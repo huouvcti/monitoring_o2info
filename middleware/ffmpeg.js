826f341cb5a705d53b5d445fd51c458f6cd797ac
huouvcti <huouvcti@gmail.com>
2023-02-02 16:34:58 +0900

    ff

--- middleware/ffmpeg.js
+++ middleware/ffmpeg.js
@@ -13,3 +13,5 @@ stream = new Stream({
 // ffmpeg -rtsp_transport tcp -i rtsp://210.99.70.120:1935/live/cctv009.stream -vcodec libx264 -hls_flags delete_segments  test.m3u8
 
 // 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
+
+//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
